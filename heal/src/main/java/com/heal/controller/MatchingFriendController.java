aaa63ab056ed01c292c1140b700ffc685161049b
jelly02 <haribo__@naver.com>
2021-10-29 23:39:36 +0900

    Add : 이미지 서버 저장

--- heal/src/main/java/com/heal/controller/MatchingFriendController.java
+++ heal/src/main/java/com/heal/controller/MatchingFriendController.java
@@ -1,12 +1,31 @@
 package com.heal.controller;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.lang3.RandomStringUtils;
+import com.heal.dto.Profile;
+import com.heal.service.MatchingFriendService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class MatchingFriendController {
+	
+	
+	 	@Autowired
+	 	private MatchingFriendService service;
 
 		/** 친구 찾기 페이지 첫 화면 */
 		@RequestMapping("/friend/friendHome")
@@ -21,6 +40,36 @@ public class MatchingFriendController {
 			
 			return "/friend/myProfile";
 		}
+		
+		/** 내 프로필 저장*/
+		@RequestMapping("/saveProfile")
+		public String saveProfile(Profile profile,  HttpServletRequest request, HttpSession session, Model model,
+				@RequestPart(value="file") MultipartFile file) throws Exception{
+			
+			String id= (String)session.getAttribute("id");
+			System.out.println(profile+", "+id);
+			
+			//저장할 파일
+			 String sourceFileName = file.getOriginalFilename(); 
+		     String sourceFileNameExtension = FilenameUtils.getExtension(sourceFileName).toLowerCase(); 
+		     File destinationFile; 
+		     String destinationFileName;
+		    
+		     //상대 경로 설정
+		     String fileUrl = request.getSession().getServletContext().getRealPath("profile/");
+		     
+		    	     do { 
+		    	            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + sourceFileNameExtension; 
+		    	            destinationFile = new File(fileUrl + destinationFileName); 
+		    	        } while (destinationFile.exists()); 
+		    	        
+		    	        destinationFile.getParentFile().mkdirs(); 
+		    	        file.transferTo(destinationFile); 
+		    	        
+		    	        service.boardInsertService(profile);
+		    	        
+		    	        return "/friend/friendHome";
+		    }
 		
 		/** 친구 리스트 보기 */
 		@RequestMapping("/friend/friendList")
