dd5b2b2018d426271e23c215c57e5c7bd6cf41ec
jelly02 <haribo__@naver.com>
2021-11-04 05:33:19 +0900

    ADD : CLOVA API 연동 성공

--- heal/src/main/java/com/heal/controller/MatchingFriendController.java
+++ heal/src/main/java/com/heal/controller/MatchingFriendController.java
@@ -1,11 +1,13 @@
 package com.heal.controller;
 
-import java.io.File;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.swing.filechooser.FileSystemView;
 
@@ -18,6 +20,13 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.apache.commons.lang3.RandomStringUtils;
+//json parser
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+//
 import com.heal.dto.Member;
 import com.heal.dto.Profile;
 import com.heal.service.MatchingFriendService;
@@ -121,51 +130,51 @@ public class MatchingFriendController {
 		    }
 		
 		/**내 프로필 수정*/
-		@RequestMapping("/editProfile")
-		public String editProfile(Profile profile,  HttpServletRequest request, HttpSession session, Model model,
-				@RequestPart(value="file") MultipartFile files) throws Exception{
-			
-			String id= (String)session.getAttribute("id");
-			profile.setId(id);
-			
-			//연령대 불러오기
-			String age = friendService.findAge(id);
-			profile.setAge(age);
-			
-			//닉 불러오기
-			String nick = friendService.findNick(id);
-			profile.setNick(nick);
-			
-			/** 파일 업로드 */
-			 String fileName  = files.getOriginalFilename(); 
-		     String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
-		     File destinationFile; 
-		     String destinationFileName;
-		     String fileUrl = request.getSession().getServletContext().getRealPath("profile/");
-		     
-		    	     do { 
-		    	            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension; 
-		    	            destinationFile = new File(fileUrl + destinationFileName); 
-		    	        } while (destinationFile.exists()); 
-		    	        
-		    	        destinationFile.getParentFile().mkdirs(); 
-		    	        files.transferTo(destinationFile); 
-		    	        
-		    	        profile.setFileName(destinationFileName);
-		    	        profile.setFileOriName(fileName);
-		    	        profile.setFileUrl(fileUrl);
-		    	        
-		    	        int result = friendService.updateProfile(profile);
-		    	        
-		    	        if(result==1) {
-		    				return "redirect:/friend/friendHome";
-		    			}else {
-		    				model.addAttribute("title","프로필 수정 실패");
-		    				model.addAttribute("message", "프로필 수정을 실패하였습니다. 관리자에게 문의하거나 다시 시도해주세요");
-		    				return "error";
-		    			}
-		    }
-		
+//		@RequestMapping("/editProfile")
+//		public String editProfile(Profile profile,  HttpServletRequest request, HttpSession session, Model model,
+//				@RequestPart(value="file") MultipartFile files) throws Exception{
+//			
+//			String id= (String)session.getAttribute("id");
+//			profile.setId(id);
+//			
+//			//연령대 불러오기
+//			String age = friendService.findAge(id);
+//			profile.setAge(age);
+//			
+//			//닉 불러오기
+//			String nick = friendService.findNick(id);
+//			profile.setNick(nick);
+//			
+//			/** 파일 업로드 */
+//			 String fileName  = files.getOriginalFilename(); 
+//		     String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
+//		     File destinationFile; 
+//		     String destinationFileName;
+//		     String fileUrl = request.getSession().getServletContext().getRealPath("profile/");
+//		     
+//		    	     do { 
+//		    	            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension; 
+//		    	            destinationFile = new File(fileUrl + destinationFileName); 
+//		    	        } while (destinationFile.exists()); 
+//		    	        
+//		    	        destinationFile.getParentFile().mkdirs(); 
+//		    	        files.transferTo(destinationFile); 
+//		    	        
+//		    	        profile.setFileName(destinationFileName);
+//		    	        profile.setFileOriName(fileName);
+//		    	        profile.setFileUrl(fileUrl);
+//		    	        
+//		    	        int result = friendService.updateProfile(profile);
+//		    	        
+//		    	        if(result==1) {
+//		    				return "redirect:/friend/friendHome";
+//		    			}else {
+//		    				model.addAttribute("title","프로필 수정 실패");
+//		    				model.addAttribute("message", "프로필 수정을 실패하였습니다. 관리자에게 문의하거나 다시 시도해주세요");
+//		    				return "error";
+//		    			}
+//		    }
+//		
 		/** 내 프로필 삭제 */
 		@RequestMapping("/deleteProfile")
 		public String profileDelete(String id,  Model model) {
@@ -175,6 +184,7 @@ public class MatchingFriendController {
 			id = id.trim();
 			
 			int result = friendService.deleteForm(id);
+			System.out.println(result);
 						
 			if(result==1) {
 				return "redirect:/friend/friendHome";
@@ -299,83 +309,138 @@ public class MatchingFriendController {
 		
 		
 		/** API TEST */
-		@RequestMapping("/editProfile2")
-		public void editProfile2(Profile profile,  HttpServletRequest request, HttpSession session, Model model,
-				@RequestPart(value="file") MultipartFile files) {
-			
-	        StringBuffer reqStr = new StringBuffer();
-	        String clientId = "t6b3hcxzXWbixb_bNEZX";//애플리케이션 클라이언트 아이디값";
-	        String clientSecret = "tmOhv7VXcc";//애플리케이션 클라이언트 시크릿값";
-			
-				String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
-			    String basePath = rootPath + "/";
-			    String filePath = basePath + "/" + files.getOriginalFilename();
-			
-			    System.out.println("rootpath : "+rootPath+", basePath :"+basePath+", filePath"+filePath );
-			
 
-		        try {
-		            String paramName = "image"; // 파라미터명은 image로 지정
-		            String imgFile = filePath;
-		            File uploadFile = new File(imgFile);
-		            //String apiURL = "https://openapi.naver.com/v1/vision/celebrity"; // 유명인 얼굴 인식
-		            String apiURL = "https://openapi.naver.com/v1/vision/face"; // 얼굴 감지
-		            URL url = new URL(apiURL);
-		            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		            con.setUseCaches(false);
-		            con.setDoOutput(true);
-		            con.setDoInput(true);
-		            // multipart request
-		            String boundary = "---" + System.currentTimeMillis() + "---";
-		            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-		            con.setRequestProperty("X-Naver-Client-Id", clientId);
-		            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-		            OutputStream outputStream = con.getOutputStream();
-		            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
-		            String LINE_FEED = "\r\n";
-		            // file 추가
-		            String fileName = uploadFile.getName();
-		            writer.append("--" + boundary).append(LINE_FEED);
-		            writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
-		            writer.append("Content-Type: "  + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
-		            writer.append(LINE_FEED);
-		            writer.flush();
-		            FileInputStream inputStream = new FileInputStream(uploadFile);
-		            byte[] buffer = new byte[4096];
-		            int bytesRead = -1;
-		            while ((bytesRead = inputStream.read(buffer)) != -1) {
-		                outputStream.write(buffer, 0, bytesRead);
-		            }
-		            outputStream.flush();
-		            inputStream.close();
-		            writer.append(LINE_FEED).flush();
-		            writer.append("--" + boundary + "--").append(LINE_FEED);
-		            writer.close();
-		            BufferedReader br = null;
-		            int responseCode = con.getResponseCode();
-		            if(responseCode==200) { // 정상 호출
-		                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		            } else {  // 에러 발생
-		                System.out.println("error!!!!!!! responseCode= " + responseCode);
-		                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		            }
-		            String inputLine;
-		            if(br != null) {
-		                StringBuffer response = new StringBuffer();
-		                while ((inputLine = br.readLine()) != null) {
-		                    response.append(inputLine);
-		                }
-		                br.close();
-		                System.out.println(response.toString());
-		            } else {
-		                System.out.println("error !!!");
-		            }
-		        } catch (Exception e) {
-		            System.out.println(e);
-		        }
-			
-			
-		}
+		/**내 프로필 수정*/
+				@RequestMapping("/editProfile")
+				public String editProfile2(Profile profile,  HttpServletResponse response_error, HttpServletRequest request, HttpSession session, Model model,
+						@RequestPart(value="file") MultipartFile files) throws Exception{
+					
+					String id= (String)session.getAttribute("id");
+					profile.setId(id);
+					
+					//연령대 불러오기
+					String age = friendService.findAge(id);
+					profile.setAge(age);
+					
+					//닉 불러오기
+					String nick = friendService.findNick(id);
+					profile.setNick(nick);
+					
+					/** 파일 업로드 */
+					 String fileName  = files.getOriginalFilename(); 
+				     String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
+				     File destinationFile; 
+				     String destinationFileName;
+				     String fileUrl = request.getSession().getServletContext().getRealPath("profile/");
+				     
+				    	     do { 
+				    	            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension; 
+				    	            destinationFile = new File(fileUrl + destinationFileName); 
+				    	        } while (destinationFile.exists()); 
+				    	        
+				    	        destinationFile.getParentFile().mkdirs(); 
+				    	        files.transferTo(destinationFile); 
+				    	          	        
+				    	        //api 테스트	        
+				    	        StringBuffer reqStr = new StringBuffer();
+				    	        String clientId = "t6b3hcxzXWbixb_bNEZX";//애플리케이션 클라이언트 아이디값";
+				    	        String clientSecret = "tmOhv7VXcc";//애플리케이션 클라이언트 시크릿값";
+				    	        
+				    	        try {
+				    	            String paramName = "image"; // 파라미터명은 image로 지정
+				    	            File uploadFile = new File(fileUrl + destinationFileName); 
+				    	            String apiURL = "https://openapi.naver.com/v1/vision/face"; // 얼굴 감지
+				    	            URL url = new URL(apiURL);
+				    	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+				    	            con.setUseCaches(false);
+				    	            con.setDoOutput(true);
+				    	            con.setDoInput(true);
+				    	            // multipart request
+				    	            String boundary = "---" + System.currentTimeMillis() + "---";
+				    	            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+				    	            con.setRequestProperty("X-Naver-Client-Id", clientId);
+				    	            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+				    	            OutputStream outputStream = con.getOutputStream();
+				    	            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+				    	            String LINE_FEED = "\r\n";
+				    	            // file 추가
+				    	            String fileName2 = uploadFile.getName();
+				    	            writer.append("--" + boundary).append(LINE_FEED);
+				    	            writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName2 + "\"").append(LINE_FEED);
+				    	            writer.append("Content-Type: "  + URLConnection.guessContentTypeFromName(fileName2)).append(LINE_FEED);
+				    	            writer.append(LINE_FEED);
+				    	            writer.flush();
+				    	            FileInputStream inputStream = new FileInputStream(uploadFile);
+				    	            byte[] buffer = new byte[4096];
+				    	            int bytesRead = -1;
+				    	            while ((bytesRead = inputStream.read(buffer)) != -1) {
+				    	                outputStream.write(buffer, 0, bytesRead);
+				    	            }
+				    	            outputStream.flush();
+				    	            inputStream.close();
+				    	            writer.append(LINE_FEED).flush();
+				    	            writer.append("--" + boundary + "--").append(LINE_FEED);
+				    	            writer.close();
+				    	            BufferedReader br = null;
+				    	            int responseCode = con.getResponseCode();
+				    	            if(responseCode==200) { // 정상 호출
+				    	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				    	            } else {  // 에러 발생
+				    	                System.out.println("error!!!!!!! responseCode= " + responseCode);
+				    	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				    	            }
+				    	            String inputLine;
+				    	            if(br != null) {
+				    	                StringBuffer response = new StringBuffer();
+				    	                while ((inputLine = br.readLine()) != null) {
+				    	                    response.append(inputLine);
+				    	                }
+				    	                br.close();
+				    	                
+				    	                    
+				    	                String jsonText = response.toString();
+				    	                
+				    	                JSONParser jsonParser = new JSONParser();
+				    	                JSONObject jsonObject = (JSONObject) jsonParser.parse(jsonText);
+				    	                JSONObject dataObject = (JSONObject) jsonObject.get("info");
+
+				    	                int faceCount =  Integer.parseInt(String.valueOf(dataObject.get("faceCount")));
+				    	                System.out.println("얼굴 인식 결과"+faceCount);
+				    	                
+				    	                //얼굴이 나와있는 정상 프로필 사진 전송
+				    	                if(faceCount == 1) {
+				    	                	
+				    	                	 	profile.setFileName(destinationFileName);
+				    			    	        profile.setFileOriName(fileName);
+				    			    	        profile.setFileUrl(fileUrl);
+				    			    	        
+				    			    	        int result = friendService.updateProfile(profile);
+				    			    	        
+				    			    	        if(result==1) {
+				    			    				return "/friend/friendHome";
+				    			    			}else {
+				    			    				model.addAttribute("title","프로필 수정 실패");
+				    			    				model.addAttribute("message", "프로필 수정을 실패하였습니다. 관리자에게 문의하거나 다시 시도해주세요");
+				    			    				return "error";
+				    			    			}		    	                	
+				    	                }else {
+				    	                	//얼굴이 없는 사진 경우 
+				    	                	response_error.setContentType("text/html;charset=utf-8");
+				    	        			PrintWriter out = response_error.getWriter();
+				    	        			out.println("<script>alert('[안내] 얼굴이 정확하게 나온 프로필 사진을 올려주세요.'); </script>");
+				    	        			out.flush();
+
+				    	        			return "/friend/editProfile";
+				    	                }
+				    	            } else {
+				    	                System.out.println("error !!!");
+				    	            }
+				    	        } catch (Exception e) {
+				    	            System.out.println(e);
+				    	        }    
+				    	        
+				    	        return "/friend/friendHome";
+				    } //함수 끝
 		
 	
 }
