bd467a499301ce70e69372af4f0bf4b87f4458e8
jelly02 <haribo__@naver.com>
2021-11-04 00:04:20 +0900

    Edit : 친구 매칭 컨트롤러 백업

--- heal/src/main/java/com/heal/controller/MatchingFriendController.java
+++ heal/src/main/java/com/heal/controller/MatchingFriendController.java
@@ -5,9 +5,10 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.swing.filechooser.FileSystemView;
+
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,11 +18,13 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.apache.commons.lang3.RandomStringUtils;
-
 import com.heal.dto.Member;
 import com.heal.dto.Profile;
 import com.heal.service.MatchingFriendService;
-
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -292,6 +295,86 @@ public class MatchingFriendController {
 		public String error() {
 			
 			return "/user/error";
+		}
+		
+		
+		/** API TEST */
+		@RequestMapping("/editProfile2")
+		public void editProfile2(Profile profile,  HttpServletRequest request, HttpSession session, Model model,
+				@RequestPart(value="file") MultipartFile files) {
+			
+	        StringBuffer reqStr = new StringBuffer();
+	        String clientId = "t6b3hcxzXWbixb_bNEZX";//애플리케이션 클라이언트 아이디값";
+	        String clientSecret = "tmOhv7VXcc";//애플리케이션 클라이언트 시크릿값";
+			
+				String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+			    String basePath = rootPath + "/";
+			    String filePath = basePath + "/" + files.getOriginalFilename();
+			
+			    System.out.println("rootpath : "+rootPath+", basePath :"+basePath+", filePath"+filePath );
+			
+
+		        try {
+		            String paramName = "image"; // 파라미터명은 image로 지정
+		            String imgFile = filePath;
+		            File uploadFile = new File(imgFile);
+		            //String apiURL = "https://openapi.naver.com/v1/vision/celebrity"; // 유명인 얼굴 인식
+		            String apiURL = "https://openapi.naver.com/v1/vision/face"; // 얼굴 감지
+		            URL url = new URL(apiURL);
+		            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		            con.setUseCaches(false);
+		            con.setDoOutput(true);
+		            con.setDoInput(true);
+		            // multipart request
+		            String boundary = "---" + System.currentTimeMillis() + "---";
+		            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+		            con.setRequestProperty("X-Naver-Client-Id", clientId);
+		            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+		            OutputStream outputStream = con.getOutputStream();
+		            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+		            String LINE_FEED = "\r\n";
+		            // file 추가
+		            String fileName = uploadFile.getName();
+		            writer.append("--" + boundary).append(LINE_FEED);
+		            writer.append("Content-Disposition: form-data; name=\"" + paramName + "\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+		            writer.append("Content-Type: "  + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+		            writer.append(LINE_FEED);
+		            writer.flush();
+		            FileInputStream inputStream = new FileInputStream(uploadFile);
+		            byte[] buffer = new byte[4096];
+		            int bytesRead = -1;
+		            while ((bytesRead = inputStream.read(buffer)) != -1) {
+		                outputStream.write(buffer, 0, bytesRead);
+		            }
+		            outputStream.flush();
+		            inputStream.close();
+		            writer.append(LINE_FEED).flush();
+		            writer.append("--" + boundary + "--").append(LINE_FEED);
+		            writer.close();
+		            BufferedReader br = null;
+		            int responseCode = con.getResponseCode();
+		            if(responseCode==200) { // 정상 호출
+		                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		            } else {  // 에러 발생
+		                System.out.println("error!!!!!!! responseCode= " + responseCode);
+		                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		            }
+		            String inputLine;
+		            if(br != null) {
+		                StringBuffer response = new StringBuffer();
+		                while ((inputLine = br.readLine()) != null) {
+		                    response.append(inputLine);
+		                }
+		                br.close();
+		                System.out.println(response.toString());
+		            } else {
+		                System.out.println("error !!!");
+		            }
+		        } catch (Exception e) {
+		            System.out.println(e);
+		        }
+			
+			
 		}
 		
 	
